5657dcfe6afe8308f1e88a028fc51dcd3a92afb4
possumlabscrypto <contact@possumlabs.io>
2025-03-16 23:18:03 +0100

    initial commit

--- src/TopCutMarket.sol
+++ src/TopCutMarket.sol
@@ -1,7 +1,7 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity =0.8.19;
 
-contract Counter {
+contract TopCutMarket {
     uint256 public number;
 
     function setNumber(uint256 newNumber) public {
